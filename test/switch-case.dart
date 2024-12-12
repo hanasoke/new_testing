c41ad390971ee7c8aadb65773a254b686a5de65c
hanasoke@gmail.com <hanasoke@gmail.com>
2024-12-12 13:36:54 +0700

    switch-case

---
+++ test/switch-case.dart
@@ -0,0 +1,70 @@
+void main() {
+  // int gfg = 5;
+  // switch (gfg) {
+  //   case 1:
+  //     {
+  //       print("Hanamiku number 1");
+  //     }
+  //     break;
+  //   case 2:
+  //     {
+  //       print("Hanasuki number 2");
+  //     }
+  //     break;
+  //   case 3:
+  //     {
+  //       print("Hanasuba number 3");
+  //     }
+  //     break;
+  //   case 4:
+  //     {
+  //       print("Hananino number 4");
+  //     }
+  //   case 5:
+  //     {
+  //       print("Hanachika number 5");
+  //     }
+  //   default:
+  //     {
+  //       print("This is default case");
+  //     }
+  //     break;
+  // }
+
+  int girl = 1;
+  String nakano = "Miku";
+  switch (girl) {
+    case 1:
+      {
+        switch (nakano) {
+          case 'Miku':
+            {
+              print("Hello Nakano Miku");
+            }
+        }
+      }
+      break;
+    case 2:
+      {
+        switch (nakano) {
+          case 'Itsuki':
+            {
+              print("Hello Nakano Itsuki");
+            }
+        }
+      }
+      break;
+    case 3:
+      {
+        print("Nakano Yotsuba");
+      }
+    case 4:
+      {
+        print("Nakano Nino");
+      }
+    case 5:
+      {
+        print("Nakano Ichika");
+      }
+  }
+}
